837e109224815a2c083d8198eb5ec249a7d17713
wangyonghao <wangyonghao@rongcloud.cn>
2019-06-14 14:17:53 +0800

    feat: es6 适配 es5 语法

--- src/admin-report.js
+++ src/admin-report.js
@@ -1,19 +1,19 @@
 /* 
 此文件实现融云注册统计功能, 开发者可忽略
  */
-const https = require('https'),
+var https = require('https'),
   qs = require('querystring');
 
-const DEMO_TYPE = 2; // SealRTC
+var DEMO_TYPE = 2; // SealRTC
 
-const ADMIN_REPORT_URL = 'admin.rongcloud.cn',
+var ADMIN_REPORT_URL = 'admin.rongcloud.cn',
   ADMIN_REPORT_PATH = '/demoApi/sendData';
 
-const request = (options) => {
+var request = (options) => {
   return new Promise((resolve, reject) => {
-    let content = options.content;
+    var content = options.content;
 
-    let req = https.request(options, function (res) {
+    var req = https.request(options, function (res) {
       res.setEncoding('utf8');
       res.on('data', function (chunk) {
         resolve(chunk);
@@ -28,9 +28,9 @@ const request = (options) => {
   });
 };
 
-const reportRegister = (phone, region) => {
-  let content = `mobile=${phone}&demo_type=${DEMO_TYPE}&region=${region}`;
-  let options = {
+var reportRegister = (phone, region) => {
+  var content = `mobile=${phone}&demo_type=${DEMO_TYPE}&region=${region}`;
+  var options = {
     hostname: ADMIN_REPORT_URL,
     path: ADMIN_REPORT_PATH,
     method: 'POST',
