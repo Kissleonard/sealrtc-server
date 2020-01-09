1ac6f11961530bd087636d203cf9f2f7237c7ddb
yuhongda <yuhongda@rongcloud.cn>
2020-01-09 08:23:13 +0800

    feat: 增加配置项

--- src/conf.js
+++ src/conf.js
@@ -3,6 +3,8 @@ module.exports = {
   RONGCLOUD_APPKEY: '',
   // Secret 可在融云开发者后台获取 https://developer.rongcloud.cn
   RONGCLOUD_SECRET: '',
+  // RongCloud Server API 地址
+  RONGCLOUD_SERVER_API: '',
   // 调试模式不发送验证码，点击发送验证码后输入万能验证码 9999 可通过验证
   DEBUG: true,
   // https://www.yunpian.com/entry DEBUG 为 true 此项可为空
